3f34aed4e831b0e22b49f8ab99a742bd4751efe5
Luise Cederkvist Christiansen <vfj276@bayes.sund.root.ku.dk>
2016-06-20 16:25:55 +0200

    rcpp attributes updated (Rcpp::attributesCompile())

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,46 +6,86 @@
 
 using namespace Rcpp;
 
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP ex_rcpparma_hello_world() {
+// pn
+double pn(mat y, mat mu, mat sigma);
+RcppExport SEXP mcif_pn(SEXP ySEXP, SEXP muSEXP, SEXP sigmaSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
-    __result = Rcpp::wrap(rcpparma_hello_world());
+    Rcpp::traits::input_parameter< mat >::type y(ySEXP);
+    Rcpp::traits::input_parameter< mat >::type mu(muSEXP);
+    Rcpp::traits::input_parameter< mat >::type sigma(sigmaSEXP);
+    __result = Rcpp::wrap(pn(y, mu, sigma));
     return __result;
 END_RCPP
 }
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP ex_rcpparma_outerproduct(SEXP xSEXP) {
+// loglikfull
+vec loglikfull(mat y, mat b, mat u, mat sigma, mat alph, mat dalph, bool cond);
+RcppExport SEXP mcif_loglikfull(SEXP ySEXP, SEXP bSEXP, SEXP uSEXP, SEXP sigmaSEXP, SEXP alphSEXP, SEXP dalphSEXP, SEXP condSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    __result = Rcpp::wrap(rcpparma_outerproduct(x));
+    Rcpp::traits::input_parameter< mat >::type y(ySEXP);
+    Rcpp::traits::input_parameter< mat >::type b(bSEXP);
+    Rcpp::traits::input_parameter< mat >::type u(uSEXP);
+    Rcpp::traits::input_parameter< mat >::type sigma(sigmaSEXP);
+    Rcpp::traits::input_parameter< mat >::type alph(alphSEXP);
+    Rcpp::traits::input_parameter< mat >::type dalph(dalphSEXP);
+    Rcpp::traits::input_parameter< bool >::type cond(condSEXP);
+    __result = Rcpp::wrap(loglikfull(y, b, u, sigma, alph, dalph, cond));
     return __result;
 END_RCPP
 }
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP ex_rcpparma_innerproduct(SEXP xSEXP) {
+// Dloglikfull
+mat Dloglikfull(mat y, mat b, mat u, mat sigma, mat alph, mat dalph, bool cond);
+RcppExport SEXP mcif_Dloglikfull(SEXP ySEXP, SEXP bSEXP, SEXP uSEXP, SEXP sigmaSEXP, SEXP alphSEXP, SEXP dalphSEXP, SEXP condSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    __result = Rcpp::wrap(rcpparma_innerproduct(x));
+    Rcpp::traits::input_parameter< mat >::type y(ySEXP);
+    Rcpp::traits::input_parameter< mat >::type b(bSEXP);
+    Rcpp::traits::input_parameter< mat >::type u(uSEXP);
+    Rcpp::traits::input_parameter< mat >::type sigma(sigmaSEXP);
+    Rcpp::traits::input_parameter< mat >::type alph(alphSEXP);
+    Rcpp::traits::input_parameter< mat >::type dalph(dalphSEXP);
+    Rcpp::traits::input_parameter< bool >::type cond(condSEXP);
+    __result = Rcpp::wrap(Dloglikfull(y, b, u, sigma, alph, dalph, cond));
     return __result;
 END_RCPP
 }
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP ex_rcpparma_bothproducts(SEXP xSEXP) {
+// D2loglikfull
+mat D2loglikfull(mat y, mat b, mat u, mat sigma, mat alph, mat dalph, bool cond);
+RcppExport SEXP mcif_D2loglikfull(SEXP ySEXP, SEXP bSEXP, SEXP uSEXP, SEXP sigmaSEXP, SEXP alphSEXP, SEXP dalphSEXP, SEXP condSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    __result = Rcpp::wrap(rcpparma_bothproducts(x));
+    Rcpp::traits::input_parameter< mat >::type y(ySEXP);
+    Rcpp::traits::input_parameter< mat >::type b(bSEXP);
+    Rcpp::traits::input_parameter< mat >::type u(uSEXP);
+    Rcpp::traits::input_parameter< mat >::type sigma(sigmaSEXP);
+    Rcpp::traits::input_parameter< mat >::type alph(alphSEXP);
+    Rcpp::traits::input_parameter< mat >::type dalph(dalphSEXP);
+    Rcpp::traits::input_parameter< bool >::type cond(condSEXP);
+    __result = Rcpp::wrap(D2loglikfull(y, b, u, sigma, alph, dalph, cond));
+    return __result;
+END_RCPP
+}
+// loglik
+vec loglik(mat y, mat b, mat sigma, mat alph, mat dalph, int nq, double stepsize, unsigned iter, bool debug);
+RcppExport SEXP mcif_loglik(SEXP ySEXP, SEXP bSEXP, SEXP sigmaSEXP, SEXP alphSEXP, SEXP dalphSEXP, SEXP nqSEXP, SEXP stepsizeSEXP, SEXP iterSEXP, SEXP debugSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< mat >::type y(ySEXP);
+    Rcpp::traits::input_parameter< mat >::type b(bSEXP);
+    Rcpp::traits::input_parameter< mat >::type sigma(sigmaSEXP);
+    Rcpp::traits::input_parameter< mat >::type alph(alphSEXP);
+    Rcpp::traits::input_parameter< mat >::type dalph(dalphSEXP);
+    Rcpp::traits::input_parameter< int >::type nq(nqSEXP);
+    Rcpp::traits::input_parameter< double >::type stepsize(stepsizeSEXP);
+    Rcpp::traits::input_parameter< unsigned >::type iter(iterSEXP);
+    Rcpp::traits::input_parameter< bool >::type debug(debugSEXP);
+    __result = Rcpp::wrap(loglik(y, b, sigma, alph, dalph, nq, stepsize, iter, debug));
     return __result;
 END_RCPP
 }
